1335f254cb346ac894b2d098de87a3b2a12405db
Bayes <979514428@qq.com>
2024-10-31 14:14:25 +0800

    add-generator

---
+++ sample/sample_generator.py
@@ -0,0 +1,30 @@
+#parse input 2 json
+'''
+    sample format
+    line_id \001\001\001 0:label0,1:label1,.... \001\001\001 slot_id \001\001\001 fea_attr \002\002\002 fea_attr \002\002\002 ....
+
+    hash slot   slotid -- fea_attr1,fea_attr2,fea_attr3
+    embed slot  slotid -- 1.02,2.03..(emb_length)
+'''
+
+import simplejson as json
+from . import generator_config
+class SampleGenerator(object):
+    def __init__(self):
+        self.config = generator_config.sample_config
+        pass
+
+    def generatePosSample(self):
+        sample = []
+        return sample
+
+    def generateNegSample(self, pos_sample:list)
+        sample =
+
+    def generate(self):
+        samples = []
+        samples = generatePosSample()
+        samples = generateNegSample()
+        return samples
+
+
