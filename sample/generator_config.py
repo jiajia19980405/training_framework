e811e2538824447610f0627a1b33eda65914ebfa
git <979514428@qq.com>
2024-11-20 17:40:54 +0800

    update

--- sample/generator_config.py
+++ sample/generator_config.py
@@ -1,9 +1,12 @@
 sample_config = {
     'embed_slot_id': [],
-    'polynomial_sample_size':0,
-    'item_frequency_dict_path':'',
-    'random_neg_sampel_size':0,
     'lv1_split':'\001\001\001',
     'lv2_split':'\002\002\002',
     'label_headers':['sim']
+}
+
+generator_config = {
+    'polynomial_sample_size':0,
+    'item_frequency_dict_path':'',
+    'random_neg_sampel_size':0
 }
--- sample/sample_generator.py
+++ sample/sample_generator.py
@@ -1,42 +1,66 @@
 #parse input 2 json
 '''
     sample format
-    line_id \001\001\001 0:label0;1:label1,.... \001\001\001 slot_id \001\001\001 fea_attr \002\002\002 fea_attr \002\002\002 ....
+    line_id \001 0:label0;1:label1,.... \001 slot_id \001 fea_attr \002 fea_attr \002 ....
 
     hash slot   slotid -- fea_attr1,fea_attr2,fea_attr3
     embed slot  slotid -- 1.02,2.03..(emb_length)
 '''
 from . import generator_config
-SPLIT_1 = generator_config['lv1_split']
-SPLIT_2 = generator_config['lv2_split']
 import simplejson as json
 import global_objs
 
+
 class Sample:
-    def __init__(self):
+    def __init__(self, **spliter):
         self.line_id = 'default'
         self.label = {}
-        self.feature = {} #slot_id:fea
-        self.dense_fea = {}
+        self.feature = {}  #slot_id:fea
+        self.dense_feature = {}
+        if spliter:
+            if 'lv1_split' in spliter.keys():
+                self.SPLIT_1 = spliter['lv1_split']
+            else:
+                self.SPLIT_1 = '\001'
+            if 'lv2_split' in spliter.keys():
+                self.SPLIT_2 = spliter['lv2_split']
+            else:
+                self.SPLIT_2 = '\002'
 
-    def format(self, lines:list):
-        index_lis = [(lines.index(k), v)for k,v in self.label.items() if k in lines]
-        index_lis.sort(key=lambda x:x[0])
-        label_str = ';'.join(['{}:{}'.format(k,v) for k,v in index_lis])
-        fea_str = [SPLIT_2.join(['{}:{}'.format(k,l) for l in v]) for k, v in self.feature.items()]
-        return SPLIT_1.join([self.line_id, label_str, SPLIT_1.join(fea_str)])
+    def format(self, lines: list):
+        index_lis = [(lines.index(k), v) for k, v in self.label.items() if k in lines]
+        index_lis.sort(key=lambda x: x[0])
+        label_str = ';'.join(['{}:{}'.format(k, v) for k, v in index_lis])
+        fea_str = [self.SPLIT_2.join(['{}:{}'.format(k, l) for l in v]) for k, v in self.feature.items()]
+        dense_fea_str = ['{}:{}'.format(k, ','.join(v)) for k, v in self.dense_feature.items()]
+        return self.SPLIT_1.join([self.line_id, label_str, self.SPLIT_1.join(fea_str), dense_fea_str])
+
+    def parseFromStr(self, s, label_lines: list, dense_fea_lines: list = []):
+        tmp_lis = s.split(self.SPLIT_1)
+        self.line_id = tmp_lis[0]
+
+        label_list = [x.split(':') for x in tmp_lis[1].split(';')]
+        self.label = {label_lines[v[0]]: v[1] for v in label_list}
+        fea_lis = tmp_lis[2:]
+        for i in range(0, len(fea_lis), 2):
+            if fea_lis[i] not in dense_fea_lines:
+                self.feature[fea_lis[i]] = fea_lis[i + 1].split(self.SPLIT_2)
+            else:
+                self.feature[fea_lis[i]] = fea_lis[i + 1].split(',')
+
+        return self
 
 
 class SampleGenerator(object):
     def __init__(self):
-        self.config = generator_config.sample_config
-        pass
+        self.config = generator_config.generator_config
+        self.sample_config = generator_config.sample_config
 
     def generatePosSample(self):
         sample = []
         return sample
 
-    def generateNegSample(self, pos_sample:list):
+    def generateNegSample(self, pos_sample: list):
         neg_sample = []
         return neg_sample
 
@@ -46,22 +70,9 @@ class SampleGenerator(object):
         samples = self.generateNegSample(samples)
         return samples
 
-    def parse(self, s:str):
-        tmp_lis = s.split(SPLIT_1)
-        lines = self.config['label_headers']
-
+    def parse(self, s: str):
+        lines = self.sample_config['label_headers']
+        embed_slot_id = self.sample_config['embed_slot_id']
         sample = Sample()
-        sample.line_id = tmp_lis[0]
-
-        label_list = [x.split(':') for x in tmp_lis[1].split(';')]
-        sample.label = {lines[v[0]]: v[1] for v in label_list}
-
-        fea_lis = tmp_lis[2:]
-        for i in range(0,len(fea_lis),2):
-            if fea_lis[i] not in self.config['embed_slot_id']:
-                sample.feature[fea_lis[i]] = fea_lis[i+1].split(SPLIT_2)
-            else:
-                sample.feature[fea_lis[i]] = fea_lis[i+1].split(',')
-
+        sample.parseFromStr(s, lines, embed_slot_id)
         return sample
-
