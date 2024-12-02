6b490726004f993aef7ada2687c2092e07296f77
JIA <dla@dladeMacBook-Pro.local>
2024-12-02 13:37:24 +0800

    update

--- trash_scripts/find_dataset_str.py
+++ trash_scripts/find_dataset_str.py
@@ -1,8 +1,8 @@
-import PyPDF2
 import json
 import os
 from tqdm import tqdm
 import fitz
+from collections import Counter
 '''
 S1 拿到目录 -- 只要页码
 S2 对每一篇检索 dataset/ data set 关键字，记录出现的页码，去重
@@ -11,11 +11,8 @@ S4 保存，导出一个json
 '''
 
 
-def handle_book(path, book_name):
-    file = open(path + '/' + book_name, 'rb+')
-    reader = PyPDF2.PdfFileReader(file)
+def handle_book(path, book_name, year, mark = 'sec'):
     pdf = fitz.open(path + '/' + book_name)
-    outline = reader.outlines
     papers_cate = []
     err_titles = []
     def binary_search(number, lis):
@@ -30,58 +27,60 @@ def handle_book(path, book_name):
                 left = mid + 1
         return -1
 
-    def single_layer_get(bookmark_list, reader):
+    def single_layer_get(bookmark_list, year, mark = 'sec'):
         lis = []
-        for item in bookmark_list:
-            if isinstance(item, PyPDF2.generic._data_structures.Destination):
-                page_num = reader.getDestinationPageNumber(item)
-                lis.append(page_num)
+        for level, title, page in bookmark_list:
+            if title.startswith(mark + year + '-'):
+                lis.append(page)
         return lis
-
-    def save_pdf(sp, ep, title):
-        writer = PyPDF2.PdfFileWriter()
-        for page_num in range(sp, ep):
-            page_in = reader.pages[page_num]
-            writer.appendPagesFromReader(reader)
-
-        with open(path + '/' + book_name + '_res_papers/' + title.replace('/', '') + '.pdf', 'wb') as wf:
-            writer.write(wf)
 
     def save_pdf2(sp, ep, title):
         new_doc = fitz.open()
         # 添加指定范围内的页面到新文档
-        #for page_num in range(sp, ep):
+        if ep - sp > 20:
+            ep = sp + 20 # bug: bad item
         new_doc.insert_pdf(pdf, from_page=sp, to_page=ep)
         new_doc.save(path + '/' + book_name + '_res_papers/' + title.replace('/', '') + '.pdf')
 
     def get_paper_obj(index, lis, papers_cate, starts, err_titles):
         titles = [t['title'] for t in papers_cate]
-        title = '_'.join(reader.getPage(lis[index]).extractText().split(' ')[:10]).replace('\n', '')[:40]
+        title = '_'.join(pdf.get_page_text(lis[index]).split(' ')[:10]).replace('\n', '')[:40]
         if title in titles:
             return
-        paper_end = lis[index + 1]
-        papers_cate.append({'title': title, 'start': lis[index], 'end': paper_end, 'in_reviwer': lis[index] - starts + 1})
+        papers_cate.append({'title': title, 'start': lis[index], 'end': lis[index + 1], 'in_reviwer': lis[index] - starts + 1})
         try:
             save_pdf2(lis[index], lis[index+1] - 1, title)
         except Exception as e:
             err_titles.append(title)
 
+    #kw = ['dataset', 'data set', 'database']
+    kw = ['database']
 
     # S1 拿到所有起始页面
-    papers_start_pages = []
-    for line in outline:
-        papers_start_pages.extend(single_layer_get(line, reader))
-    papers_start_pages.append(reader.numPages)
+    book_marks = pdf.get_toc()
+    papers_start_pages = single_layer_get(book_marks, year, mark = mark)
+    papers_start_pages.append(pdf.page_count)
+    papers_start_pages = [i - 1 for i in papers_start_pages]
 
     # S2 保存含有dataset / data set的页码
     start_page = papers_start_pages[0]
     founds = []
+    news = []
     print('start to find in each paper\n')
     for page in tqdm(list(range(start_page, papers_start_pages[-1]))):
-        this_page_text = reader.getPage(page).extractText().replace('\n', '').lower()
-        if 'dataset' in this_page_text or 'data set' in this_page_text:
-            founds.append(page)
-
+        this_page_text = pdf.get_page_text(page).replace('\n', '').lower()
+        for it in kw:
+            if it in this_page_text:
+                founds.append(page)
+                words = this_page_text.split(' ')
+                for i,word in enumerate(words):
+                    if word == it:
+                        news.append(words[i - 1] + ' ' + words[i])
+                break
+    print(len(founds))
+    print(founds)
+    print(news)
+    return founds, news
 
     if not os.path.exists(path + '/' + book_name + '_res_papers'):
         os.makedirs(path + '/' + book_name + '_res_papers')
@@ -99,4 +98,21 @@ def handle_book(path, book_name):
     with open(path + '/' + book_name + '_res_papers/final_err.json', 'w+') as f:
         f.write(json.dumps(err_titles))
 
-handle_book('/Users/admin/Downloads/', 'sec20-full_proceedings.pdf')
+    print('{} has {} papers contains the kw'.format(book_name, len(papers_cate)))
+
+#book_names = ['sec24_full_proceedings.pdf']
+years = [str(20 + i) for i in range(5)]
+counter = Counter([])
+book_names = ['sec20_full_proceedings.pdf', 'sec21_full_proceedings.pdf', 'sec22_full_proceedings.pdf', 'sec23_full_proceedings.pdf', 'sec24_full_proceedings.pdf']
+for i, book_name in enumerate(book_names):
+    if years[i] == '24':
+        founds, news = handle_book('/Users/dla/Downloads/', book_name, years[i], mark = 'usenixsecurity')
+        counter += Counter(news)
+    else:
+        founds, news = handle_book('/Users/dla/Downloads/', book_name, years[i])
+        counter += Counter(news)
+
+with open('/Users/dla/Downloads/counter.json', 'w+') as f:
+    f.write(json.dumps(counter))
+print(counter)
+
